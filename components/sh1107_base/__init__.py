5fbe06c632aedd8b36521f1ccddd2093f609ffb3
Jens-Christian Skibakk <jens.christian@skibakk.no>
2023-08-08 14:38:02 +0200

    Update __init__.py

--- components/sh1107_base/__init__.py
+++ components/sh1107_base/__init__.py
@@ -40,6 +40,6 @@ async def setup_sh1107(var, config):
         cg.add(var.init_invert(config[CONF_INVERT]))
     if CONF_LAMBDA in config:
         lambda_ = await cg.process_lambda(
-            config[CONF_LAMBDA], [(display.DisplayBufferRef, "it")], return_type=cg.void
+            config[CONF_LAMBDA], [(display.DisplayRef, "it")], return_type=cg.void
         )
         cg.add(var.set_writer(lambda_))
